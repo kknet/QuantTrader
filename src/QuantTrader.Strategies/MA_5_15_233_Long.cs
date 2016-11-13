e65e2c38d3a9c03ca32757b3f9a33918a6045b9c
chenxinyong <chenxinyong@gmail.com>
2016-11-13 22:01:55 +0800

    add files

---
+++ src/QuantTrader.Strategies/MA_5_15_233_Long.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuantTrader.Strategies
+{
+    public class MA_5_15_233_Long 
+    {
+
+    }
+}
